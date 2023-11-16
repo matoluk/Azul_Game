cce53ba747894fd5628d391f605a79c1f633c9cd
Viktor Samuel Podhradský <samkopodhradsky@gmail.com>
2023-11-16 22:31:10 +0100

    Fix setup in GameFnishedTest and refactor GameFinished class into correct package

--- src/test/java/sk/uniba/fmph/dcs/GameFinishedTest.java
+++ src/test/java/sk/uniba/fmph/dcs/GameFinishedTest.java
@@ -1,5 +1,6 @@
 package sk.uniba.fmph.dcs;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
@@ -7,10 +8,24 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GameFinishedTest {
+    private Optional<Tile>[][] wall;
+
+    @BeforeEach
+    public void setUp() {
+        wall = new Optional[Tile.values().length - 1][Tile.values().length - 1];
+
+        for (int i = 0; i < wall.length; i++) {
+            for (int j = 0; j < wall[i].length; j++) {
+                wall[i][j] = Optional.empty();
+            }
+        }
+    }
 
     @Test
     public void testGameFinishedWithCompleteHorizontalLine() {
-        Optional<Tile>[][] wall = createCompleteHorizontalLineWall();
+        for (int i = 0; i < Tile.values().length - 1; i++) {
+            wall[0][i] = Optional.of(Tile.values()[i]);
+        }
 
         FinishRoundResult result = GameFinished.gameFinished(wall);
 
@@ -19,40 +34,16 @@ public class GameFinishedTest {
 
     @Test
     public void testGameFinishedWithIncompleteHorizontalLine() {
-        Optional<Tile>[][] wall = createIncompleteHorizontalLineWall();
+        wall[0][0] = Optional.of(Tile.values()[0]);
+        wall[0][1] = Optional.of(Tile.values()[1]);
 
         FinishRoundResult result = GameFinished.gameFinished(wall);
-
         assertEquals(FinishRoundResult.NORMAL, result);
     }
 
     @Test
     public void testGameFinishedWithEmptyWall() {
-        Optional<Tile>[][] wall = createEmptyWall();
-
         FinishRoundResult result = GameFinished.gameFinished(wall);
-
         assertEquals(FinishRoundResult.NORMAL, result);
-    }
-
-    private Optional<Tile>[][] createCompleteHorizontalLineWall() {
-        Optional<Tile>[][] wall = new Optional[Tile.values().length - 1][Tile.values().length - 1];
-
-        for (int i = 0; i < Tile.values().length - 1; i++) {
-            wall[0][i] = Optional.of(Tile.values()[i]);
-        }
-        return wall;
-    }
-
-    private Optional<Tile>[][] createIncompleteHorizontalLineWall() {
-        Optional<Tile>[][] wall = new Optional[Tile.values().length - 1][Tile.values().length - 1];
-
-        wall[0][0] = Optional.of(Tile.values()[0]);
-        wall[0][1] = Optional.of(Tile.values()[1]);
-        return wall;
-    }
-
-    private Optional<Tile>[][] createEmptyWall() {
-        return new Optional[Tile.values().length - 1][Tile.values().length - 1];
     }
 }
