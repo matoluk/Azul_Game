1b4a4fdfb5efb78677abff1115e4eda7287f0114
Dam297 <regesdamian@gmail.com>
2023-11-16 23:24:37 +0100

    Created concrete asserts for test of class GameObserver

--- src/test/java/sk/uniba/fmph/dcs/GameObserverTest.java
+++ src/test/java/sk/uniba/fmph/dcs/GameObserverTest.java
@@ -28,5 +28,32 @@ public class GameObserverTest {
 
     @Test
     public void testObserver() {
+
+        gameObserver.notifyEverybody("");
+        assertNull("Concrete observer's state at start should be empty", observer1.getState());
+        gameObserver.registerObserver(observer1);
+        gameObserver.registerObserver(observer2);
+        assertNull("Concrete observer's state at start should be empty", observer1.getState());
+        gameObserver.notifyEverybody("Start game");
+        assertEquals("Observer's state after notify should something contains", "Start game", observer1.getState());
+        assertEquals("Observer's state after notify should something contains", "Start game", observer2.getState());
+        assertNull("Concrete observer's state at start should be empty", observer3.getState());
+        gameObserver.cancelObserver(observer3);
+        gameObserver.notifyEverybody("Start new game");
+        assertEquals("Observer's state after notify should contains notified state", "Start new game", observer1.getState());
+        assertEquals("Observer's state after notify should contains notified state", "Start new game", observer2.getState());
+        gameObserver.cancelObserver(observer1);
+        gameObserver.registerObserver(observer3);
+        gameObserver.notifyEverybody("End of game");
+        assertEquals("Observer's state after cancelling shouldn't changed", "Start new game", observer1.getState());
+        assertEquals("Observer's state after notify should contains notified state", "End of game", observer2.getState());
+        assertEquals("Observer's state after notify should contains notified state", "End of game", observer3.getState());
+        gameObserver.cancelObserver(observer2);
+        gameObserver.cancelObserver(observer3);
+        gameObserver.registerObserver(observer1);
+        gameObserver.notifyEverybody("Player 2 won game");
+        assertEquals("Observer's state after notify should contains notified state", "Player 2 won game", observer1.getState());
+        assertEquals("Observer's state after cancelling shouldn't changed", "End of game", observer2.getState());
+        assertEquals("Observer's state after cancelling shouldn't changed", "End of game", observer3.getState());
     }
 }
