5cb900b6c075f9d0b072937dc5e44ce4685fb19f
Viktor Samuel Podhradský <samkopodhradsky@gmail.com>
2023-11-16 22:03:56 +0100

    Implemet rule for getting points from collection all five tiles from one color

---
+++ src/main/java/sk/uniba/fmph/dcs/FullColorRule.java
@@ -0,0 +1,29 @@
+package sk.uniba.fmph.dcs;
+
+import java.util.Optional;
+
+public class FullColorRule implements ScoringRule{
+    @Override
+    public int calculatePoints(Optional<Tile>[][] wall){
+        int sum = 0;
+
+        for (int i = 0; i < wall.length; i++) {
+            int inOneColor = 0;
+
+            for (int j = 0; j < wall.length; j++) {
+                int rowIndex = j;
+                int colIndex = (i + j) % wall.length;
+
+                if (wall[rowIndex][colIndex].isPresent()) {
+                    inOneColor++;
+                } else {
+                    inOneColor = 0;
+                }
+            }
+            if (inOneColor == wall.length) {
+                sum += 10;
+            }
+        }
+        return sum;
+    }
+}
