b211de4c0118cb0700cc960c38f15244c1ac4734
Ondrej Škorňák <130708513+OndrejSkornak@users.noreply.github.com>
2023-11-17 12:07:19 +0100

    Initial commit of UsedTiles class skeleton with method stubs.

---
+++ src/main/java/sk/uniba/fmph/dcs/UsedTiles.java
@@ -0,0 +1,25 @@
+package sk.uniba.fmph.dcs;
+
+import java.util.*;
+
+public class UsedTiles {
+    private List<Tile> UsedTiles;
+
+    public UsedTiles() {
+        // Constructor implementation
+    }
+
+    public void give(Tile[] tiles) {
+        // Implementation
+    }
+
+    public Tile[] takeAll() {
+        // Implementation
+        return null;
+    }
+
+    public String state() {
+        // Implementation
+        return null;
+    }
+}
