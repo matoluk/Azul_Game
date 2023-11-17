4e4196c43c134962f56c9cd7f28f18fa9abbe26e
Ondrej Škorňák <130708513+OndrejSkornak@users.noreply.github.com>
2023-11-17 12:36:07 +0100

    Implemented the give method for UsedTiles class. Implemented the takeAll method for UsedTiles class. Implemented the state method for UsedTiles class.

--- src/main/java/sk/uniba/fmph/dcs/UsedTiles.java
+++ src/main/java/sk/uniba/fmph/dcs/UsedTiles.java
@@ -3,23 +3,30 @@ package sk.uniba.fmph.dcs;
 import java.util.*;
 
 public class UsedTiles {
-    private List<Tile> UsedTiles;
+    private List<Tile> usedTiles; //List to hold used tiles
 
     public UsedTiles() {
-        // Constructor implementation
+        this.usedTiles = new ArrayList<>();
     }
 
+    // Method to add an array of Tile objects to the usedTiles list.
     public void give(Tile[] tiles) {
-        // Implementation
+        this.usedTiles.addAll(Arrays.asList(tiles));
     }
 
+    // Method to retrieve and remove all Tile objects from the usedTiles list.
     public Tile[] takeAll() {
-        // Implementation
-        return null;
+        Tile[] tilesArray = new Tile[this.usedTiles.size()];
+        tilesArray = this.usedTiles.toArray(tilesArray);
+        this.usedTiles.clear();
+        return tilesArray;
     }
 
+    // Method to return a string representation of the current state of usedTiles.
     public String state() {
-        // Implementation
-        return null;
+        return "UsedTiles{" +
+                "count=" + usedTiles.size() +
+                ", usedTiles=" + usedTiles +
+                '}';
     }
 }
