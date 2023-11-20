5259fae8357537b834d69c3c9aa0bf4ecc8eb9f1
Peter Komoras <komoraspeter@gmail.com>
2023-11-20 12:58:10 +0100

    created TyleSource.java

---
+++ src/main/java/sk/uniba/fmph/dcs/TyleSource.java
@@ -0,0 +1,11 @@
+package sk.uniba.fmph.dcs;
+
+abstract class TyleSource {
+    abstract Tile[] take(int idx);
+
+    abstract boolean isEmpty();
+
+    abstract void startNewRound();
+
+    abstract String string();
+}
