d1486687f8465a837491b9fcd3da22f49e9bd17b
Zrubak4 <fifo.zrubak4@gmail.com>
2023-11-20 21:37:23 +0100

    Create TableArea class to mediate between the game and individual source tiles + added tests

---
+++ src/test/java/sk/uniba/fmph/dcs/TableAreaTest.java
@@ -0,0 +1,95 @@
+package sk.uniba.fmph.dcs;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Collection;
+
+
+
+
+
+public class TableAreaTest {
+
+
+    @Test
+    public void testTakeValidSource() {
+
+        TableCenter tableCenter = new TableCenter();
+        Tile[] tilesToAdd = new Tile[]{Tile.RED, Tile.RED, Tile.RED};
+        tableCenter.add(tilesToAdd);
+        TableArea tableArea = new TableArea(new ArrayList<>(Collections.singletonList(tableCenter)));
+
+        Tile[] takenTiles = tableArea.take(0, Tile.RED.ordinal());
+
+        assertEquals(tilesToAdd, takenTiles);
+        assertTrue(tableCenter.isEmpty());
+    }
+
+
+
+    @Test
+    public void testTakeInvalidSource() {
+        TableCenter tableCenter = new TableCenter();
+        TableArea tableArea = new TableArea(new ArrayList<TyleSource>((Collection<? extends TyleSource>) tableCenter));
+
+        Tile[] takenTiles = tableArea.take(1, Tile.RED.ordinal());
+
+        assertEquals(0, takenTiles.length);
+    }
+
+
+
+    @Test
+    public void testIsRoundEndEmptySources() {
+        TableCenter source1 = new TableCenter();
+        TableCenter source2 = new TableCenter();
+        ArrayList<TyleSource> sources = new ArrayList<>(Arrays.asList(source1, source2));
+        TableArea tableArea = new TableArea(sources);
+
+        assertTrue(tableArea.isRoundEnd());
+    }
+
+
+    @Test
+    public void testIsRoundEndNonEmptySources() {
+        TableCenter tableCenter = new TableCenter();
+        tableCenter.add(new Tile[]{Tile.RED});
+        TableArea tableArea = new TableArea(new ArrayList<>(Collections.singletonList(tableCenter)));
+
+
+        assertFalse(tableArea.isRoundEnd());
+    }
+
+
+    @Test
+    public void testStartNewRound() {
+        TableCenter source1 = new TableCenter();
+        source1.add(new Tile[]{Tile.RED, Tile.RED, Tile.RED});
+        TableCenter source2 = new TableCenter();
+        ArrayList<TyleSource> sources = new ArrayList<>(Arrays.asList(source1, source2));
+        TableArea tableArea = new TableArea(sources);
+
+        tableArea.startNewRound();
+
+        assertTrue(source1.isEmpty());
+        assertTrue(source2.isEmpty());
+    }
+
+    @Test
+    public void testState() {
+        TableCenter source1 = new TableCenter();
+        source1.add(new Tile[]{Tile.RED, Tile.BLACK, Tile.RED});
+        TableCenter source2 = new TableCenter();
+        source2.add(new Tile[]{Tile.GREEN, Tile.GREEN, Tile.GREEN});
+        ArrayList<TyleSource> sources = new ArrayList<>(Arrays.asList(source1, source2));
+        TableArea tableArea = new TableArea(sources);
+
+        String expectedState = source1.state() + "\n" + source2.state() + "\n";
+
+        assertEquals(expectedState, tableArea.state());
+    }
+}
