faf5d1d6ac8c78d927310d94144ad4e9e85f22f7
Peter Komoras <komoraspeter@gmail.com>
2023-11-20 21:19:10 +0100

    change HashMap<Tile, Integer> numOfTiles to ArrayList<Tile> tiles;

--- src/main/java/sk/uniba/fmph/dcs/TableCenter.java
+++ src/main/java/sk/uniba/fmph/dcs/TableCenter.java
@@ -1,58 +1,62 @@
 package sk.uniba.fmph.dcs;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class TableCenter extends TyleSource {
 
     /** STARTING_PLAYER=0, RED=1, GREEN=2, YELLOW=3, BLUE=4, BLACK=5 */
-    HashMap<Integer, Tile> intToTile;
-    HashMap<Tile, Integer> numOfTiles;
+    private final HashMap<Integer, Tile> idxToTile;
+    private final ArrayList<Tile> tiles;
     public TableCenter() {
-        this.intToTile = new HashMap<>();
-        for (Tile t: Tile.values()) intToTile.put(t.ordinal(), t);
-
-        this.numOfTiles = new HashMap<>();
-        numOfTiles.put(Tile.STARTING_PLAYER, 1);
+        this.idxToTile = new HashMap<>();
+        for (Tile t: Tile.values()) idxToTile.put(t.ordinal(), t);
+        this.tiles = new ArrayList<>();
+        tiles.add(Tile.STARTING_PLAYER);
     }
     @Override
-    Tile[] take(int idx) {
+    public Tile[] take(int idx) {
         ArrayList<Tile> result = new ArrayList<>();
-        if (numOfTiles.containsKey(intToTile.get(idx))) {
-            Tile tile = intToTile.get(idx);
-            for (int i = 0; i < numOfTiles.get(tile); i++) {
-                result.add(tile);
+        if (idx < 0 || 5 < idx ) return new Tile[0];
+        if (tiles.contains(idxToTile.get(idx))) {
+            Tile givenTile = idxToTile.get(idx);
+            for (Tile t: tiles) {
+                if (t.equals(givenTile)) result.add(givenTile);
             }
-            numOfTiles.remove(tile);
+            tiles.removeIf(t -> t.equals(givenTile));
         }
-        if (numOfTiles.get(Tile.STARTING_PLAYER) == 1) {
+
+        if (tiles.contains(Tile.STARTING_PLAYER)) {
             result.add(Tile.STARTING_PLAYER);
-            numOfTiles.remove(Tile.STARTING_PLAYER);
+            tiles.remove(Tile.STARTING_PLAYER);
         }
+
         return result.toArray(new Tile[result.size()]);
     }
 
     @Override
-    boolean isEmpty() {
-        return numOfTiles.isEmpty();
+    public boolean isEmpty() {
+        return tiles.isEmpty();
     }
 
     @Override
-    void startNewRound() {
-        numOfTiles.clear();
-        numOfTiles.put(Tile.STARTING_PLAYER, 1);
+    public void startNewRound() {
+        tiles.clear();
+        tiles.add(Tile.STARTING_PLAYER);
     }
 
     @Override
-    String state() {
-        return numOfTiles.toString();
-    }
-
-    public void add(Tile[] tiles) {
-        for (Tile t: tiles) {
-            if (numOfTiles.containsKey(t)) numOfTiles.put(t, numOfTiles.get(t) + 1);
-            else numOfTiles.put(t, 1);
+    public String state() {
+        StringBuilder toReturn = new StringBuilder();
+        for (final Tile tile : tiles) {
+            toReturn.append(tile.toString());
         }
+        return toReturn.toString();
+    }
+
+    public void add(Tile[] newTiles) {
+        tiles.addAll(new ArrayList<>(Arrays.asList(newTiles)));
     }
 
 }
