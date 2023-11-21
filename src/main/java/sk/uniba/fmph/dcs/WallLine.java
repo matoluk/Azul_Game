58f69e74444bdb2ad3bd02cdb0abfd3fa7a43c79
SamuelTarhaj <samo.tarhaj111@gmail.com>
2023-11-21 01:01:02 +0100

    implemented putTile

--- src/main/java/sk/uniba/fmph/dcs/WallLine.java
+++ src/main/java/sk/uniba/fmph/dcs/WallLine.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Optional;
 
-public class WallLine {
+public class WallLine implements WallLineInterface{
     private ArrayList tiles;
     private int countColours;
     private WallLine lineUp;
@@ -39,45 +39,74 @@ public class WallLine {
         return line;
     }
 
-//    public Points putTile(Tile tile){
-//        int p = 0;
-//        int idx = tiles.indexOf(tile);
-//        if(canPutTile(tile)) {
-//            currentWallLine[idx] = tile;
-//            p++;
-//            if(idx != 0){
-//                int i = idx - 1;
-//                while(i >= 0 & currentWallLine[i] != null) {
-//                    p++;
-//                    i--;
-//                }
-//            }
-//            if(idx != countColours - 1){
-//                int i = idx + 1;
-//                while(i <= countColours - 1  & currentWallLine[i] != null){
-//                    p++;
-//                    i++;
-//                }
-//            }
-//            if(lineUp != null){
-//                Optional<Tile>[] up = lineUp.getTiles();
-//                if(up[idx].isPresent()){
-//                    p+=columnUp(lineUp, idx, p);
-//                }
-//            }
-//        }
-//
-//    }
-//    private int columnUp(WallLine wallLine, int idx, int p){
-//        if(wallLine.lineUp != null){
-//            Optional<Tile>[] up = wallLine.lineUp.getTiles();
-//            if(up[idx].isPresent()){
-//                p++;
-//                p+=columnUp(wallLine.lineUp, idx, p);
-//                return p;
-//            }
-//        }
-//    }
+    public Points putTile(Tile tile){
+        int p = 0;
+        int k = 0;
+        int idx = tiles.indexOf(tile);
+        if(canPutTile(tile)) {
+            currentWallLine[idx] = tile;
+            p++;
+            k = 1;
+            if(idx != 0){
+                int i = idx - 1;
+                while(i >= 0 & currentWallLine[i] != null) {
+                    p++;
+                    i--;
+                    k = 0;
+                }
+            }
+            if(idx != countColours - 1){
+                int i = idx + 1;
+                while(i <= countColours - 1  & currentWallLine[i] != null){
+                    p++;
+                    i++;
+                    k = 0;
+                }
+            }
+            if(lineDown != null){
+                Optional<Tile>[] down = lineDown.getTiles();
+                if(down[idx].isPresent()){
+                    p++;
+                    k = 1;
+                    p += columnDown(lineDown, idx, p);
+                }
+            }
+            if(lineUp != null){
+                Optional<Tile>[] up = lineUp.getTiles();
+                if(up[idx].isPresent()){
+                    if(k == 0){
+                        p++;
+                    }
+                    p += columnUp(lineUp, idx, p);
+                }
+            }
+        }
+        return new Points(p);
+    }
+    private int columnUp(WallLine wallLine, int idx, int p){
+        if(wallLine.lineUp != null){
+            Optional<Tile>[] up = wallLine.lineUp.getTiles();
+            if(up[idx].isPresent()){
+                p++;
+                p = columnUp(wallLine.lineUp, idx, p);
+                return p;
+           }
+            return p;
+        }
+        return p;
+    }
+    private int columnDown(WallLine wallLine, int idx, int p){
+        if(wallLine.lineDown != null){
+            Optional<Tile>[] down = wallLine.lineDown.getTiles();
+            if(down[idx].isPresent()){
+                p++;
+                p = columnDown(wallLine.lineDown, idx, p);
+                return p;
+            }
+            return p;
+        }
+        return p;
+    }
 
     public String state(){
         StringBuilder toReturn = new StringBuilder();
